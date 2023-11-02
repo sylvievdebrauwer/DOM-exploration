f9c38a2d2dfe4263380babf4e97febe52025033e
sylvievdebrauwer <sylvie.debrauwer@gmail.com>
2023-11-02 12:24:19 +0100

    added changes

--- 2.Selectors/script.js
+++ 2.Selectors/script.js
@@ -8,12 +8,13 @@ console.log(importantElements);
 
 // Iterate over the important elements and add a title attribute to each one.
 for (const element of importantElements) {
-    element.setAttribute("title", "This is an important element.");
+    element.setAttribute("aria-label", "This is an important element.");
   }
-
 
 
 
 // TODO: Select all the img tags and loop through them. If they have no important class, turn their display property to none
 // TODO: Loop through all the paragraphs and display their content in the console. If the paragraph has a class, display its classname as well
 // TODO: Give each of the paragraph a random text color (different for each one) UNLESS it has a class then leave it as it is.
+
+
