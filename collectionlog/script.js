06122f8509d61b28fc6ff09035e9a73a775a759c
sylvievdebrauwer <sylvie.debrauwer@gmail.com>
2023-11-14 18:47:18 +0100

    div and js cards added

---
+++ collectionlog/script.js
@@ -0,0 +1,93 @@
+const collection = [
+    {
+      name: 'Pulp Fiction',
+      director: 'Quentin Tarantino',
+      releaseYear: 1994,
+      picture: 'link/to/a/picture',
+      genre: ['Crime', 'Drama'],
+      cast: ['John Travolta', 'Samuel L Jackson', 'Uma Thurman', 'Amanda Plummer']
+    },
+
+    {
+        name: 'Pulp Fiction',
+        director: 'Quentin Tarantino',
+        releaseYear: 1994,
+        picture: 'link/to/a/picture',
+        genre: ['Crime', 'Drama'],
+        cast: ['John Travolta', 'Samuel L Jackson', 'Uma Thurman', 'Amanda Plummer']
+    },
+
+    {
+        name: 'Pulp Fiction',
+        director: 'Quentin Tarantino',
+        releaseYear: 1994,
+        picture: 'link/to/a/picture',
+        genre: ['Crime', 'Drama'],
+        cast: ['John Travolta', 'Samuel L Jackson', 'Uma Thurman', 'Amanda Plummer']
+    },
+
+    {
+        name: 'Pulp Fiction',
+        director: 'Quentin Tarantino',
+        releaseYear: 1994,
+        picture: 'link/to/a/picture',
+        genre: ['Crime', 'Drama'],
+        cast: ['John Travolta', 'Samuel L Jackson', 'Uma Thurman', 'Amanda Plummer']
+    },
+
+    {
+        name: 'Pulp Fiction',
+        director: 'Quentin Tarantino',
+        releaseYear: 1994,
+        picture: 'link/to/a/picture',
+        genre: ['Crime', 'Drama'],
+        cast: ['John Travolta', 'Samuel L Jackson', 'Uma Thurman', 'Amanda Plummer']
+    },
+
+    {
+        name: 'Pulp Fiction',
+        director: 'Quentin Tarantino',
+        releaseYear: 1994,
+        picture: 'link/to/a/picture',
+        genre: ['Crime', 'Drama'],
+        cast: ['John Travolta', 'Samuel L Jackson', 'Uma Thurman', 'Amanda Plummer']
+    },
+
+    {
+        name: 'Pulp Fiction',
+        director: 'Quentin Tarantino',
+        releaseYear: 1994,
+        picture: 'link/to/a/picture',
+        genre: ['Crime', 'Drama'],
+        cast: ['John Travolta', 'Samuel L Jackson', 'Uma Thurman', 'Amanda Plummer']
+    },
+
+    {
+        name: 'Pulp Fiction',
+        director: 'Quentin Tarantino',
+        releaseYear: 1994,
+        picture: 'link/to/a/picture',
+        genre: ['Crime', 'Drama'],
+        cast: ['John Travolta', 'Samuel L Jackson', 'Uma Thurman', 'Amanda Plummer']
+    },
+
+    {
+        name: 'Pulp Fiction',
+        director: 'Quentin Tarantino',
+        releaseYear: 1994,
+        picture: 'link/to/a/picture',
+        genre: ['Crime', 'Drama'],
+        cast: ['John Travolta', 'Samuel L Jackson', 'Uma Thurman', 'Amanda Plummer']
+    },
+    
+    {
+        name: 'Pulp Fiction',
+        director: 'Quentin Tarantino',
+        releaseYear: 1994,
+        picture: 'link/to/a/picture',
+        genre: ['Crime', 'Drama'],
+        cast: ['John Travolta', 'Samuel L Jackson', 'Uma Thurman', 'Amanda Plummer']
+    },
+  
+    // ...
+  ]
